69a5a01de56ee1524d7ba9492dc756a53dec9862
Beyram <beyram.taglietti@gmail.com>
2024-04-07 17:55:58 +0200

    feat: add migration files and update db schema

---
+++ src/drizzle/migrations/0000_lush_namor.sql
@@ -0,0 +1,97 @@
+CREATE TABLE IF NOT EXISTS "friend_requests" (
+	"sender_id" uuid NOT NULL,
+	"receiver_id" uuid NOT NULL,
+	CONSTRAINT "friend_requests_sender_id_receiver_id_pk" PRIMARY KEY("sender_id","receiver_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "friends" (
+	"user_id" uuid NOT NULL,
+	"friend_id" uuid NOT NULL,
+	CONSTRAINT "friends_user_id_friend_id_pk" PRIMARY KEY("user_id","friend_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "tasks" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"value" boolean DEFAULT false NOT NULL,
+	"description" text,
+	"date" timestamp with time zone,
+	"trip_id" uuid NOT NULL,
+	"price" integer
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "trip_partecipants" (
+	"user_id" uuid NOT NULL,
+	"trip_id" uuid NOT NULL,
+	CONSTRAINT "trip_partecipants_trip_id_user_id_pk" PRIMARY KEY("trip_id","user_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "trips" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"background" text,
+	"date" timestamp with time zone NOT NULL,
+	"owner_id" uuid NOT NULL,
+	"background_provider" varchar(255)
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"email" varchar(100) NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"refresh_token" varchar(255) NOT NULL,
+	"profile_picture" varchar(255) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "User_email_key" ON "users" ("email");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "User_username_key" ON "users" ("username");--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "friend_requests" ADD CONSTRAINT "friend_requests_sender_id_users_id_fk" FOREIGN KEY ("sender_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "friend_requests" ADD CONSTRAINT "friend_requests_receiver_id_users_id_fk" FOREIGN KEY ("receiver_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "friends" ADD CONSTRAINT "friends_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "friends" ADD CONSTRAINT "friends_friend_id_users_id_fk" FOREIGN KEY ("friend_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "tasks" ADD CONSTRAINT "tasks_trip_id_trips_id_fk" FOREIGN KEY ("trip_id") REFERENCES "trips"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "trip_partecipants" ADD CONSTRAINT "trip_partecipants_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "trip_partecipants" ADD CONSTRAINT "trip_partecipants_trip_id_trips_id_fk" FOREIGN KEY ("trip_id") REFERENCES "trips"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "trips" ADD CONSTRAINT "trips_owner_id_users_id_fk" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -11,22 +11,6 @@ import {
   varchar,
 } from 'drizzle-orm/pg-core';
 
-export const prismaMigrations = pgTable('_prisma_migrations', {
-  id: varchar('id', { length: 36 }).primaryKey().notNull(),
-  checksum: varchar('checksum', { length: 64 }).notNull(),
-  finishedAt: timestamp('finished_at', { withTimezone: true, mode: 'string' }),
-  migrationName: varchar('migration_name', { length: 255 }).notNull(),
-  logs: text('logs'),
-  rolledBackAt: timestamp('rolled_back_at', {
-    withTimezone: true,
-    mode: 'string',
-  }),
-  startedAt: timestamp('started_at', { withTimezone: true, mode: 'string' })
-    .defaultNow()
-    .notNull(),
-  appliedStepsCount: integer('applied_steps_count').default(0).notNull(),
-});
-
 export const users = pgTable(
   'users',
   {
